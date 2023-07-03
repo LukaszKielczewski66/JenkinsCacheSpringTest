68e57b16f9637ca5064df37babee346ac8fe0810
LukaszKielczewski66 <LukaszKielczewski66@gmail.com>
2023-07-03 12:11:54 +0200

    first commit

---
+++ src/main/java/com/jenkins/jenkinscachespringtest/JenkinsCacheSpringTestApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinscachespringtest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsCacheSpringTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JenkinsCacheSpringTestApplication.class, args);
+    }
+
+}
